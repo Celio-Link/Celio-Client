a45e7428dabf3b4cb08fe6e83912d0cde719301b
Nils Kirchhof <nils.kirchhof@t-online.de>
2026-02-10 18:44:00 +0100

    Change fav icon and title

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('linkClientNext');
+  protected readonly title = signal('Celio Link');
 }
